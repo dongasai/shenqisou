143b78430e1e6a7e497b2e55858be614cca93aca
dongasai <1514582970@qq.com>
2023-09-03 03:24:39 +0800

    init #2

---
+++ app/Models/Bt.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+/**
+ * @property int $id
+ * @property int $name
+ *
+ *
+ * php artisan scout:import "App\Models\Bt"
+ */
+class Bt extends Model
+{
+    use HasFactory;
+    protected $table='bt';
+    use \Laravel\Scout\Searchable;
+
+    /**
+     * 获取与模型关联的索引的名称。
+     *
+     * @return string
+     */
+    public function searchableAs()
+    {
+        return 'ttt_index';
+    }
+
+
+
+    /**
+     * 获取模型的可索引的数据。
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        $array = $this->toArray();
+
+        // 自定义数据数组...
+
+        return [
+            'name'=>$this->name,
+            'id'=>$this->id
+        ];
+    }
+
+
+    /**
+     * 获取用于索引模型的值
+     *
+     * @return mixed
+     */
+    public function getScoutKey()
+    {
+        return $this->id;
+    }
+
+    /**
+     * 获取用于索引模型的键名
+     *
+     * @return mixed
+     */
+    public function getScoutKeyName()
+    {
+        return 'id';
+    }
+
+
+
+
+
+}
