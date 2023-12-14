226c8e000e7de109a030de322ba081248dd98e6b
dongasai <1514582970@qq.com>
2023-12-14 23:49:52 +0800

    打开链接

--- app/Admin/Repositories/SearchRepository.php
+++ app/Admin/Repositories/SearchRepository.php
@@ -19,16 +19,49 @@ class SearchRepository extends EloquentRepository
         $this->setPaginate($model);
         // 获取筛选参数
         $search = $model->filter()->input('search', '');
-
+        /**
+         * > 3712474
+         * 1 => 1880046
+         * 2 => 1997554
+         * 3 => 2481351
+         * 4 => 2310427
+         * 5 => 3421267
+         * 6 => 435672
+         * 7 => 1947258
+         * 8 => 2843356
+         * 9 => 2833853
+         * 10 => 3375574
+         * 11 => 947040
+         * 12 => 1223421
+         * 13 => 1676783
+         * 14 => 3247731
+         * 15 => 3353140
+         * 16 => 2700512
+         * 17 => 2797618
+         * 18 => 993035
+         * 19 => 3358369
+         * 20 => 967763
+         * 21 => 3455610
+         * 22 => 649859
+         * 23 => 894675
+         * 24 => 1952943
+         * 25 => 2658116
+         * 26 => 3194591
+         */
 //        dump($search,$this->eloquentClass);
         if($search){
             //            dd($ids);
-            $ids= Search::query($this->eloquentClass,$search);
+            $ids= Search::queryIndex2Id($this->eloquentClass,$search);
             if(!$ids){
                 $ids = [-1];
             }
             $query = $this->newQuery()->whereIn('id',$ids);
-
+            $_sort = request('_sort');
+            if (!$_sort){
+//                dump("没有排序");
+                $idsString = implode(',',$ids);
+                $query->orderByRaw("FIELD( id, $idsString )");
+            }
         }else{
             $query = $this->newQuery();
 
--- app/Helper/Search.php
+++ app/Helper/Search.php
@@ -2,6 +2,9 @@
 
 namespace App\Helper;
 
+use Laravel\Scout\EngineManager;
+use Laravel\Scout\Scout;
+
 class Search
 {
     /**
@@ -14,7 +17,25 @@ class Search
     {
         return Cache::cacheCall([__CLASS__, __FILE__, $model, $q], function ($model, $search) {
             return $model::search($search)->paginate(9999, 'a', 1)->pluck('id');
-        }, [$model, $q], 600);
+        }, [$model, $q], 1);
+    }
+
+
+    static public function queryIndex2Id($model, $q)
+    {
+        return Cache::cacheCall([__CLASS__, __FILE__, $model, $q], function ( $model, $search) {
+
+            /**
+             * @var \Illuminate\Pagination\Paginator $res
+             */
+            $res =  $model::search($search)->simplePaginateRaw(9999, 'a', 1);
+
+            $ids = array_column($res->items()['hits'],'id');
+
+//            dump($ids);
+//             dd($res->items(),$ids);
+             return $ids;
+        }, [$model, $q], 1);
     }
 
 }
